451fd8978be059a308cd106caec2c1009c1d04e8
alexgcoleman <107187057+alexgcoleman@users.noreply.github.com>
2024-11-13 13:30:43 +1100

    paycode definitions llm

--- src/organic_company_history/basis.py
+++ src/organic_company_history/basis.py
@@ -6,6 +6,7 @@ from pandera.polars import DataFrameModel
 from .polars_llm import PolarsLLM
 
 NUM_EMPLOYEES = 5
+MIN_UNIQUE_PAYCODES = 18
 
 
 class HR(DataFrameModel):
@@ -33,9 +34,28 @@ class Payroll(DataFrameModel):
         coerce = True
 
 
+class PayrollDefinitions(DataFrameModel):
+    pay_code: pa.String
+    pay_code_description: pa.String
+    pay_category: pa.String
+
+    class Config:
+        coerce = True
+
+
 class HRExpert(PolarsLLM):
     def make_message_content(self) -> str:
         return f"Generate data for {NUM_EMPLOYEES} employees for a knitting company"
+
+
+class PayCodeDefinitionsExpert(PolarsLLM):
+    def make_message_content(self) -> str:
+        return (
+            "Generate a paycode mapping file that we can use to set up a "
+            "payroll system for a knitting company. This should include all paycodes "
+            "we would expect to pay to our employees. Different leave types should use "
+            f"different paycodes. There should be at least {MIN_UNIQUE_PAYCODES} different paycodes."
+        )
 
 
 class PayrollExpert(PolarsLLM):
@@ -55,6 +75,12 @@ hr_expert = HRExpert(
     response_parser=lambda df: df.with_columns(pl.col("hire_date").str.to_date()),
 )
 
+paycode_definitions_expert = PayCodeDefinitionsExpert(
+    name="ac-knitting-paycode",
+    expertise="Payroll Systems and Administration",
+    schema=PayrollDefinitions,
+)
+
 
 payroll_expert = PayrollExpert(
     name="ac-knitting-payroll",
@@ -68,6 +94,10 @@ payroll_expert = PayrollExpert(
 hr_data = hr_expert.get_dataframe()
 
 print(f"\n\nhr_data:\n{hr_data}")
+
+paycode_data = paycode_definitions_expert.get_dataframe()
+
+print(f"\n\npaycode_data:\n{paycode_data}")
 
 payroll_data = pl.concat(
     payroll_expert.get_dataframe(
