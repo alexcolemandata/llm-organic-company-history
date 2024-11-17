e12bb5afbb53b9d09d62ff06b7aaec98455d22dc
alexgcoleman <110796341+alexcolemandata@users.noreply.github.com>
2024-11-17 22:22:42 +1100

    add main.py and a couple of demo industries

---
+++ src/organic_company_history/main.py
@@ -0,0 +1,11 @@
+"""Generate data for a few silly industries"""
+from .basis import make_data_for_industry
+
+print(f"\n\n\nGenerating data for big game hunting:\n")
+big_game_hunting = make_data_for_industry("big game hunting")
+
+print(f"\n\n\nGenerating data for interstellar coffee delivery:\n")
+big_game_hunting = make_data_for_industry("interstellar coffee delivery")
+
+print(f"\n\n\nGenerating data for knitting:\n")
+knitting_data = make_data_for_industry("knitting")
