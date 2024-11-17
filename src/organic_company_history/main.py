3f9b322753b225e9f1a6866ed80fdd2f936d28b5
alexgcoleman <110796341+alexcolemandata@users.noreply.github.com>
2024-11-17 23:05:49 +1100

    main accepts industry via command line args

--- src/organic_company_history/main.py
+++ src/organic_company_history/main.py
@@ -1,4 +1,5 @@
 """Generate and print data for a few silly industries"""
+import argparse
 from os import get_terminal_size
 
 from .basis import make_data_for_industry, GeneratedData
@@ -11,6 +12,15 @@ def demo(industry: str) -> GeneratedData:
     return make_data_for_industry(industry)
 
 
-knitting = demo("occult detective agency")
-space_coffee = demo("interstellar coffee delivery")
-knitting = demo("knitting")
+if __name__ == "__main__":
+    parser = argparse.ArgumentParser(
+        description="Generate and print data for a specified industry."
+    )
+    parser.add_argument(
+        "industry",
+        type=str,
+        help="The name of the industry for which to generate data.",
+    )
+    args = parser.parse_args()
+
+    demo(args.industry)
